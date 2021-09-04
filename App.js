62d2641d0aa19a5d3aec249188c6b2e9d6ebeaaf
victormiguelx <victor.miguelx@outlook.com>
2021-09-04 12:15:21 -0300

    my first react mobile app

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Meu primeiro Mobile App - React Native</Text>
+      <Text>Victor Miguel Gaspar de Oliveira</Text>
+      <Button title="Já sei construir meu Mobile App" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
